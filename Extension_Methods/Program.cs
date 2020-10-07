235cda7ae845162515669c47c6ef380188795e6d
Renan Beraldi <beraldi2019renan@gmail.com>
2020-10-07 18:33:31 -0300

    Program Ended

---
+++ Extension_Methods/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Extension_Methods.Extensions;
+
+public class ProcessFile
+{
+    public static void Main()
+    {
+        string s1 = "Good morning dear students";
+        Console.WriteLine(s1.Cut(10));
+    }
+}
